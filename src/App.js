5e95b3dc944455ca7326fbb96f75c5ea4b6c2281
madushanthmadu <madushanthmadu007@gmail.com>
2023-06-15 13:59:21 +0530

    first try of react and deploy-3

--- src/App.js
+++ src/App.js
@@ -42,15 +42,17 @@ class App extends Component {
         <div className='tab' style={{ display: this.state.nav_tab}}>
           <img className='tab_logo' src="https://firebasestorage.googleapis.com/v0/b/web-practices-marvel-1.appspot.com/o/Marvel_Logo.jpg?alt=media&token=27218d3e-c111-44da-8edb-74746a985fb4" alt="Logo"/>
 
-          <SearchBox
-            search='search-2'
-            placeholder='Search Movie'
-            handlechange={this.HandleChange} 
-          />
+          <div className='tab-search-box'>
+            <SearchBox
+              search='search-2'
+              placeholder='Search Movie'
+              handlechange={this.HandleChange} 
+            />
 
-          <button className='search-btn' onClick={this.hidenav}>
-            Search
-          </button>
+            <button className='search-btn' onClick={this.hidenav}>
+              Search
+            </button>
+          </div>
         </div>
 
         <div className='screen' style={{ display: this.state.nav_bar}}>
